548a783858c640c6c0ff4f558668d5461aa3ef95
nicolas <nico.ing.mec.2@gmail.com>
2024-03-05 10:18:27 -0500

    Creacion de app ToDo

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,47 @@
-import logo from '../logo.svg';
-import '../App.css';
+import React, { useState } from 'react';
+import TodoList from './TodoList';
+import '../App.css'; // Asume que tienes estilos básicos aquí
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([]); // crea un array de todos
+  const [task, setTask] = useState(""); // crea el string - se crea vacio 
+
+  const addTodo = event => { // metodo para crear una nueva tarea
+    event.preventDefault();
+    if (task.trim() !== "") { // valida que exista informacion al dar clic en add task
+      setTodos([...todos, { id: Date.now(), task, completed: false }]);   // agrega a la lista la tarea, 
+      setTask(""); //limpia el valor del input 
+    }
+  };
+
+  const toggleComplete = id => { // parametro id 
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo // valido que id con id del todo 
+      )
+    );
+  };
+
+  const removeTodo = id => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>My To-Do</h1>
+      <div className="App">
+        <form onSubmit={addTodo}>
+          <input
+            type="text"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          />
+          <button type="submit">Add Task</button>
+        </form>
+      </div>
+      <TodoList todos={todos} toggleComplete={toggleComplete} removeTodo={removeTodo} />
     </div>
   );
-}
+};
 
 export default App;
